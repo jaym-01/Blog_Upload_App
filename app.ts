9d876e146acf95cd0ac2c8452a3cb70680e94696
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-07-17 13:22:37 +0100

    clean up

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
+//get external modules
 var fr = require('fs');
 const express = require('express');
 const app = express();
-var path = require('path');
 const mul = require('multer');
 
+//get my modules
 const convert_HTML = require('./project_modules/convert_html')
 const logging = require('./project_modules/logging');
 
 const add_log = logging.add_log;
 const convertHTML = convert_HTML.convertHTML;
 
+//for recieving file from client
+//specifies where to store the file and the name it should have
 const storage = mul.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './externalfile');
@@ -19,29 +22,32 @@ const storage = mul.diskStorage({
     }
 });
 
+//post api request
 app.post('/', mul({storage: storage}).single('file'), function (req, res) {
 
-    var folder_name:string = req.body.folder_name;
-
-    //folder name validation - ensuring in only has letters and numbers
-    for(let i = 0; i < folder_name.length; i++){
-
-        let ascii = folder_name.charCodeAt(i);
-
-        if(ascii < 48 || (ascii > 57 && ascii < 65) || (ascii > 90 && ascii < 97) || ascii > 122){
-            add_log("ERROR", "File uploaded is not a .docx file or is empty");
-    
-            //respond with error
-            res.sendFile(__dirname + "/public/fail.html")
-            add_log("SUCCESS", "Sent page reporting error with upload to " + req.ip);
-
-            return;
-        }
-    } 
-
     try{
+        //name input into from from client - the folder name
+        var folder_name:string = req.body.folder_name;
+
+        //folder name validation - ensuring in only has letters and numbers
+        for(let i = 0; i < folder_name.length; i++){
+
+            let ascii = folder_name.charCodeAt(i);
+
+            if(ascii < 48 || (ascii > 57 && ascii < 65) || (ascii > 90 && ascii < 97) || ascii > 122){
+                add_log("ERROR", "File uploaded is not a .docx file or is empty");
+    
+                //respond with error
+                res.sendFile(__dirname + "/public/fail.html")
+                add_log("SUCCESS", "Sent page reporting error with upload to " + req.ip);
+
+                return;
+            }
+        } 
+
+        //check files has been uploaded and name exists
         if(req.file == null || req.file.originalname.length < 5 || req.file.originalname.substring(req.file.originalname.length - 5, req.file.originalname.length) != ".docx" || folder_name.length < 3 || folder_name.length > 25){
-            // console.log("ERROR");//log it in log file
+            
             add_log("ERROR", "File uploaded is not a .docx file or is empty");
     
             //respond with error
@@ -51,17 +57,20 @@ app.post('/', mul({storage: storage}).single('file'), function (req, res) {
         else{
             var save_path = "./SavedHTMLFiles/" + folder_name;
 
+            //create new folder for html file - remove old one if it exists
             if(fr.existsSync(save_path)){
                 fr.rmdirSync(save_path, {recursive: true});
             }
             fr.mkdirSync(save_path);
-
+            
+            //perform html conversion and send a response
             var html_code = convertHTML(req, res, save_path);
             // add_log("SUCCESS", "Converted .docx file to html");
             add_log("SUCCESS", "called function to create html file");
         }
     }
     catch(err){
+        //if any errors happen during the process - gives error
         add_log("ERROR", err.toString() + ", on api post");
         res.send("An Error has occured");
     }
